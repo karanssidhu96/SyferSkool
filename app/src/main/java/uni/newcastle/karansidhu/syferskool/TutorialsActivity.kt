b04235964fcf0d402975b79a34491b3eef0a1e5c
karanssidhu96 <karanssidhu96@gmail.com>
2018-05-09 19:33:36 +0100

    Tutorials all work to MVP standard

--- app/src/main/java/uni/newcastle/karansidhu/syferskool/TutorialsActivity.kt
+++ app/src/main/java/uni/newcastle/karansidhu/syferskool/TutorialsActivity.kt
@@ -1,10 +1,6 @@
 package uni.newcastle.karansidhu.syferskool
 
-import android.content.Context
-import android.graphics.drawable.Drawable
-import android.media.Image
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.*
 
@@ -18,6 +14,8 @@ class TutorialsActivity : AppCompatActivity()
     lateinit var explanationTextView : TextView
     lateinit var imageView : ImageView
     lateinit var testTextView : TextView
+    lateinit var inputEditText : EditText
+    lateinit var testButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +24,10 @@ class TutorialsActivity : AppCompatActivity()
         explanationTextView = findViewById(R.id.textView_tutorial_explanation)
         imageView = findViewById(R.id.imageView_tutorial)
         testTextView = findViewById(R.id.textView_tutorial_test_described)
+        inputEditText = findViewById(R.id.editText_tutoiral)
+        testButton = findViewById(R.id.button_tutorial)
         getIncomingIntent()
+        test(testButton, titleTextView.text.toString())
     }
 
     private fun getIncomingIntent()
@@ -45,6 +46,12 @@ class TutorialsActivity : AppCompatActivity()
             "Caesar" -> setTutorial(title, R.drawable.caesar,
                     getString(R.string.text_caesar_explanation),
                     getString(R.string.text_caesar_test))
+            "ROT13" -> setTutorial(title, R.drawable.rot13,
+                    getString(R.string.text_ROT13_explanation),
+                    getString(R.string.text_ROT13_test))
+            "Vigenère" -> setTutorial(title, R.drawable.vigenere,
+                    getString(R.string.text_vigenère_explanation),
+                    getString(R.string.text_vigenère_test))
         }
     }
 
@@ -54,5 +61,25 @@ class TutorialsActivity : AppCompatActivity()
         imageView.setImageResource(image)
         explanationTextView.text = explanation
         testTextView.text = test
+    }
+
+    private fun test(btn : Button, title : String)
+    {
+        val polyCiphers = PolyCiphers()
+        val shiftCiphers = ShiftCiphers()
+        var correctAnswer = ""
+        btn.setOnClickListener({
+            when (title)
+            {
+                "Caesar" -> correctAnswer = shiftCiphers.shift("emperor", 3)
+                "ROT13" -> correctAnswer = shiftCiphers.shift("thirteen", 13)
+                "Vigenère" -> correctAnswer = polyCiphers.viegenere("blaise", "key",
+                        true)
+            }
+            if (correctAnswer == inputEditText.text.toString())
+            {
+                Toast.makeText(applicationContext, "It works!", Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
